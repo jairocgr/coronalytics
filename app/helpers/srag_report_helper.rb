82dcaf8e61eba3757345653c4460719d475bf052
Jairo Rodrigues Filho <jairocgr@gmail.com>
2022-12-08 10:16:56 -0300

    More reporting data

--- app/helpers/srag_report_helper.rb
+++ app/helpers/srag_report_helper.rb
@@ -3,12 +3,14 @@ module SragReportHelper
 
   class SragReporter
 
+    MAX_MONTH = 12
+
     def deaths(year)
       srag = get_srag_from(year)
       result = []
 
-      (1..12).each do |month|
-        result.append({
+      (1..MAX_MONTH).each do |month|
+        record = ({
           month: month,
           total: SragRecord.where(srag: srag)
             .where('year(DT_EVOLUCA) = ?', year)
@@ -34,6 +36,15 @@ module SragReportHelper
             .where('VACINA_COV not in (1,2)')
             .count,
         })
+
+        total = record[:vaccinated]+record[:unvaccinated]
+        total = 1 if total == 0
+
+        record[:vaccinated_percentage] = (record[:vaccinated]*100) / total
+        record[:unvaccinated_percentage] = (record[:unvaccinated]*100) / total
+        record[:undef_percentage] = (record[:undef]*100) / record[:total]
+
+        result.append record
       end
 
       return result
@@ -41,7 +52,7 @@ module SragReportHelper
 
     def vaccination(year)
       result = []
-      (1..6).each do |month|
+      (1..MAX_MONTH).each do |month|
         vaccination = VaccinationNumber.where(year: year).where(month: month).first!
         result.append({
           month: month,
@@ -52,10 +63,88 @@ module SragReportHelper
       return result
     end
 
+    def hospitalizations(year)
+      srag = get_srag_from(year)
+      result = []
+
+      (1..MAX_MONTH).each do |month|
+        record = ({
+          month: month,
+          hospitalizations: SragRecord.where(srag: srag)
+            .where('year(DT_INTERNA) = ?', year)
+            .where('month(DT_INTERNA) = ?', month)
+            .where('HOSPITAL = 1')
+            .count,
+          hospitalizations_vaccinated: SragRecord.where(srag: srag)
+            .where('year(DT_INTERNA) = ?', year)
+            .where('month(DT_INTERNA) = ?', month)
+            .where('HOSPITAL = 1')
+            .where('VACINA_COV = 1')
+            .count,
+          hospitalizations_unvaccinated: SragRecord.where(srag: srag)
+            .where('year(DT_INTERNA) = ?', year)
+            .where('month(DT_INTERNA) = ?', month)
+            .where('HOSPITAL = 1')
+            .where('VACINA_COV = 2')
+            .count,
+          hospitalizations_undef: SragRecord.where(srag: srag)
+            .where('year(DT_INTERNA) = ?', year)
+            .where('month(DT_INTERNA) = ?', month)
+            .where('HOSPITAL = 1')
+            .where('VACINA_COV not in (1,2)')
+            .count,
+
+          icus: SragRecord.where(srag: srag)
+            .where('year(DT_ENTUTI) = ?', year)
+            .where('month(DT_ENTUTI) = ?', month)
+            .where('UTI = 1')
+            .count,
+          icus_vaccinated: SragRecord.where(srag: srag)
+            .where('year(DT_ENTUTI) = ?', year)
+            .where('month(DT_ENTUTI) = ?', month)
+            .where('UTI = 1')
+            .where('VACINA_COV = 1')
+            .count,
+          icus_unvaccinated: SragRecord.where(srag: srag)
+            .where('year(DT_ENTUTI) = ?', year)
+            .where('month(DT_ENTUTI) = ?', month)
+            .where('UTI = 1')
+            .where('VACINA_COV = 2')
+            .count,
+          icus_undef: SragRecord.where(srag: srag)
+            .where('year(DT_ENTUTI) = ?', year)
+            .where('month(DT_ENTUTI) = ?', month)
+            .where('UTI = 1')
+            .where('VACINA_COV not in (1,2)')
+            .count,
+        })
+
+        total = record[:hospitalizations_vaccinated]+record[:hospitalizations_unvaccinated]
+        total = 1 if total == 0
+        record[:hospitalizations_vaccinated_percentage] = (record[:hospitalizations_vaccinated]*100) / total
+        record[:hospitalizations_unvaccinated_percentage] = (record[:hospitalizations_unvaccinated]*100) / total
+        record[:hospitalizations_undef_percentage] = (record[:hospitalizations_undef]*100) / record[:hospitalizations]
+
+        total = record[:icus_vaccinated]+record[:icus_unvaccinated]
+        total = 1 if total == 0
+        record[:icus_vaccinated_percentage] = (record[:icus_vaccinated]*100) / total
+        record[:icus_unvaccinated_percentage] = (record[:icus_unvaccinated]*100) / total
+        record[:icus_undef_percentage] = (record[:icus_undef]*100) / record[:icus]
+
+        result.append record
+      end
+
+      return result
+    end
+
     def get_srag_from(year)
       Srag.order(release_date: 'DESC').where(year: year).first!
     end
 
+  end
+
+  def get_srag_from(year)
+    Srag.order(release_date: 'DESC').where(year: year).first!
   end
 
   def month_name(month_no)
--- app/models/srag.rb
+++ app/models/srag.rb
@@ -1,7 +1,14 @@
+
+require 'pathname'
+
 class Srag < ApplicationRecord
 
   def local_path
     Rails.root.join('storage', "srag-#{id}.csv")
+  end
+
+  def file_name
+    Pathname.new(url).basename
   end
 
 end
